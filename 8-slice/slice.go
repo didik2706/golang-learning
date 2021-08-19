641dd16884336d7288d71d10f3fcd7c5c40ee7b7
didik2706 <didikhidayat27@gmail.com>
2021-08-19 19:01:52 +0700

    belajar slice

---
+++ 8-slice/slice.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fruits := []string{"Apel", "Strowberi", "Pisang", "Mangga"}
+
+	fmt.Println(fruits[0])
+	fmt.Println(fruits[:2])
+	fmt.Println(fruits[2:][1])
+}
